9b7dbd54762d9be4ed9a17aff24afd4cf2c7e2c1
SICPRobotics <ignatiusrobotics@gmail.com>
2018-02-06 16:36:14 -0600

    Update SmartDashboard2018.java

--- SmartDashboard2018/src/SmartDashboard2018.java
+++ SmartDashboard2018/src/SmartDashboard2018.java
@@ -9,18 +9,19 @@ import javax.swing.*;
 public class SmartDashboard2018 {
 	
 	private int locationValue;
-	private final String[] ALLIANCE = {"BLUE", "RED"};
-	private String fms = "RLR";
+	private int team;
 	
 	private double nkP = 0;
 	private double nkI = 0;
 	private double nkD = 0;
 	
+	private String fms = "RLR";
+	
 	private JFrame smartDashboard = new JFrame("5822 Smart Dashboard");
 	
 	
 	
-	JComboBox<String> alliance = new JComboBox<String>(ALLIANCE);
+	
 	
 	private JPanel mainBackground = new JPanel();
 	private JPanel autoMove = new JPanel();
@@ -38,7 +39,6 @@ public class SmartDashboard2018 {
 	private JLabel encoderL = new JLabel();
 	private JLabel encoderR = new JLabel();
 	private JLabel gyro = new JLabel();
-	private JLabel pidLabel = new JLabel();
 	private JLabel fieldElementsDisplay = new JLabel();
 	private JLabel cSwitch = new JLabel();
 	private JLabel scale = new JLabel();
@@ -48,10 +48,6 @@ public class SmartDashboard2018 {
 	
 	private JTextField locationShow = new JTextField();
 	private JTextField codeShow = new JTextField();
-	private JTextField kP = new JTextField();
-	private JTextField kI = new JTextField();
-	private JTextField kD = new JTextField();
-	
 	private JTextField eL = new JTextField();
 	private JTextField eR = new JTextField();
 	
@@ -59,13 +55,15 @@ public class SmartDashboard2018 {
 	private JButton left = new JButton();
 	private JButton center = new JButton();
 	private JButton right = new JButton();
+	private JButton blueSelect = new JButton();
+	private JButton redSelect = new JButton();
 	
 	private Color wolfbyteRed = new Color(133, 6, 8);
 	private Color wolfbyteGrey = new Color(150, 150, 150);
 	
 	public SmartDashboard2018() {
 		smartDashboard.setDefaultCloseOperation(smartDashboard.EXIT_ON_CLOSE);
-		smartDashboard.setSize(500, 750);
+		smartDashboard.setSize(500, 650);
 		smartDashboard.setLayout(null);
 		smartDashboard.add(mainBackground);
 		smartDashboard.setResizable(false);
@@ -124,7 +122,7 @@ public class SmartDashboard2018 {
 		});
 		
 		
-		infoPanel.setBounds(15, 135, 460, 560);
+		infoPanel.setBounds(15, 135, 460, 460);
 		infoPanel.setLayout(null);
 		infoPanel.setBackground(wolfbyteGrey);
 		infoPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createTitledBorder("Sensors")));
@@ -132,14 +130,11 @@ public class SmartDashboard2018 {
 		infoPanel.add(encoderR);
 		infoPanel.add(eL);
 		infoPanel.add(eR);
-		infoPanel.add(kP);
-		infoPanel.add(kI);
-		infoPanel.add(kD);
 		infoPanel.add(gyro);
 		infoPanel.add(gyroShow);
-		infoPanel.add(pidLabel);
 		infoPanel.add(fieldElementsDisplay);
-		infoPanel.add(alliance);
+		infoPanel.add(blueSelect);
+		infoPanel.add(redSelect);
 		infoPanel.add(cSwitchL);
 		infoPanel.add(cSwitchR);
 		infoPanel.add(scaleL);
@@ -150,58 +145,113 @@ public class SmartDashboard2018 {
 		infoPanel.add(scale);
 		infoPanel.add(fSwitch);
 		
-		fieldElementsDisplay.setBounds(15, 335, 110, 20);
+		fieldElementsDisplay.setBounds(15, 240, 110, 20);
 		fieldElementsDisplay.setText("Field Elements");
 		fieldElementsDisplay.setBorder(BorderFactory.createRaisedBevelBorder());
 		
-		alliance.setBounds(130, 335, 110, 20);
+		blueSelect.setBounds(130, 240, 55, 20);
+		blueSelect.setText("B");
+		blueSelect.setBorder(BorderFactory.createRaisedBevelBorder());
+		blueSelect.setBackground(new Color(244, 209, 170));
+		redSelect.setBounds(185, 240, 55, 20);
+		redSelect.setText("R");
+		redSelect.setBorder(BorderFactory.createRaisedBevelBorder());
+		redSelect.setBackground(new Color(244, 209, 170));
 		
-		fSwitchL.setBounds(15, 360, 50, 50);
-		fSwitchR.setBounds(75, 360, 50, 50);
-		fSwitch.setBounds(130, 360, 110, 50);
+		blueSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		        if(e.getSource() == blueSelect) {
+		        	team = 0;
+		        	if(fms.charAt(0) == 'L') {
+		        		cSwitchL.setBackground(Color.BLUE);
+		        		cSwitchR.setBackground(Color.RED);
+		        	}
+		        	else {
+		        		cSwitchL.setBackground(Color.RED);
+		        		cSwitchR.setBackground(Color.BLUE);
+		        	}
+		        	if(fms.charAt(1) == 'L') {
+		        		scaleL.setBackground(Color.BLUE);
+		        		scaleR.setBackground(Color.RED);
+		        	}
+		        	else {
+		        		scaleL.setBackground(Color.RED);
+		        		scaleR.setBackground(Color.BLUE);
+		        	}
+		        	if(fms.charAt(2) == 'L') {
+		        		fSwitchL.setBackground(Color.BLUE);
+		        		fSwitchR.setBackground(Color.RED);
+		        	}
+		        	else {
+		        		fSwitchL.setBackground(Color.RED);
+		        		fSwitchR.setBackground(Color.BLUE);
+		        	}
+		        }
+		    }
+		});
+		redSelect.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		        if(e.getSource() == redSelect) {
+		        	team = 1;
+		        	if(fms.charAt(0) == 'L') {
+		        		cSwitchL.setBackground(Color.RED);
+		        		cSwitchR.setBackground(Color.BLUE);
+		        	}
+		        	else {
+		        		cSwitchL.setBackground(Color.BLUE);
+		        		cSwitchR.setBackground(Color.RED);
+		        	}
+		        	if(fms.charAt(1) == 'L') {
+		        		scaleL.setBackground(Color.RED);
+		        		scaleR.setBackground(Color.BLUE);
+		        	}
+		        	else {
+		        		scaleL.setBackground(Color.BLUE);
+		        		scaleR.setBackground(Color.RED);
+		        	}
+		        	if(fms.charAt(2) == 'L') {
+		        		fSwitchL.setBackground(Color.RED);
+		        		fSwitchR.setBackground(Color.BLUE);
+		        	}
+		        	else {
+		        		fSwitchL.setBackground(Color.BLUE);
+		        		fSwitchR.setBackground(Color.RED);
+		        	}
+		        }
+		    }
+		});
+		
+		fSwitchL.setBounds(15, 265, 50, 50);
+		fSwitchR.setBounds(75, 265, 50, 50);
+		fSwitch.setBounds(130, 265, 110, 50);
 		fSwitch.setBorder(BorderFactory.createRaisedBevelBorder());
 		fSwitch.setText("Other Switch");
 		
-		cSwitchL.setBounds(15, 480, 50, 50);
-		cSwitchR.setBounds(75, 480, 50, 50);
-		cSwitch.setBounds(130, 480, 110, 50);
+		cSwitchL.setBounds(15, 385, 50, 50);
+		cSwitchR.setBounds(75, 385, 50, 50);
+		cSwitch.setBounds(130, 385, 110, 50);
 		cSwitch.setBorder(BorderFactory.createRaisedBevelBorder());
 		cSwitch.setText("Your Switch");
 		
-		scaleL.setBounds(15, 420, 50, 50);
-		scaleR.setBounds(75, 420, 50, 50);
-		scale.setBounds(130, 420, 110, 50);
+		scaleL.setBounds(15, 325, 50, 50);
+		scaleR.setBounds(75, 325, 50, 50);
+		scale.setBounds(130, 325, 110, 50);
 		scale.setBorder(BorderFactory.createRaisedBevelBorder());
 		scale.setText("Scale");
 		
-		gyro.setBounds(15, 185, 100, 20);
+		gyro.setBounds(15, 100, 100, 20);
 		gyro.setText("Gyro");
 		gyro.setBorder(BorderFactory.createRaisedBevelBorder());
 		gyro.setBackground(new Color(244, 209, 170));
 		
-		gyroShow.setBounds(15, 205, 426, 115);
+		gyroShow.setBounds(15, 120, 426, 115);
 		gyroShow.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createTitledBorder("Degrees")));
 		gyroShow.setBackground(new Color(244, 209, 170));
 		gyroShow.setFont(new Font("Times New Roman", Font.BOLD, 48));
 		gyroShow.setEditable(false);
 
 		
-		pidLabel.setBounds(15, 105, 100, 20);
-		pidLabel.setText("PID");
-		pidLabel.setBorder(BorderFactory.createRaisedBevelBorder());
-		pidLabel.setBackground(new Color(244, 209, 170));
-		
-		kP.setBounds(15, 125, 100, 50);
-		kP.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createTitledBorder("KP")));
-		kP.setBackground(new Color(244, 209, 170));
-		
-		kI.setBounds(125, 125, 100, 50);
-		kI.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createTitledBorder("KI")));
-		kI.setBackground(new Color(244, 209, 170));
-		
-		kD.setBounds(235, 125, 100, 50);
-		kD.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createTitledBorder("KD")));
-		kD.setBackground(new Color(244, 209, 170));
+
 		
 		encoderL.setBounds(15, 20, 100, 25);
 		encoderL.setText("Left Encoder");
@@ -252,46 +302,26 @@ public class SmartDashboard2018 {
 		smartDashboard.setVisible(true);
 	}
 	
+	//Returns the value of where the robot is located at the start of the match
 	public int location() {
 		return locationValue;
 	}
-	public double kPreturn() {
-		try {
-			nkP = Double.valueOf(kP.getText());
-			return nkP;
-		}
-		catch(Exception e) {
-			return 0;
-		}
+	//Updates the value fms so that the field elements display is correct
+	public void updateFMS(String fms) {
+		this.fms = fms;
 	}
-	public double kIreturn() {
-		try {
-			nkI = Double.valueOf(kI.getText());
-			return nkI;
-		}
-		catch(Exception e) {
-			return 0;
-		}
+	//Shows the smart dashboard on the computer
+	public void showDashboard() {
+		smartDashboard.setVisible(true);
 	}
-	public double kDreturn() {
-		try {
-			nkD = Double.valueOf(kD.getText());
-			return nkD;
-		}
-		catch(Exception e) {
-			return 0;
-		}
-	}
-	public void setFieldDataDisplay(String fmsData) {
-		
-		fms = fmsData;
-		
-	}
-	public void update(){
+	//Updates all the components on the smart dashboard so that they are up do date
+	public void update(String fms, double leftEncoder, double rightEncoder, double gyro){
 
-		String allianceSelect = (String) alliance.getSelectedItem();
-		
-		if(allianceSelect.equals("BLUE")) {
+		eL.setText(Double.toString(leftEncoder));
+		eR.setText(Double.toString(rightEncoder));
+		gyroShow.setText(Double.toString(gyro));
+			
+		if(team == 0) {
 			if(fms.charAt(0) == 'L') {
 				cSwitchL.setBackground(Color.BLUE);
 				cSwitchR.setBackground(Color.RED);
@@ -317,7 +347,7 @@ public class SmartDashboard2018 {
 				fSwitchR.setBackground(Color.BLUE);
 			}
 		}
-		else if(allianceSelect.equals("RED")) {
+		else if(team == 1) {
 			if(fms.charAt(0) == 'L') {
 				cSwitchL.setBackground(Color.RED);
 				cSwitchR.setBackground(Color.BLUE);
